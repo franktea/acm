07ad9eb51ba10960201f592b5a0cb78e74a2bd1e
frank <could.net@gmail.com>
2016-12-18 22:15:14 +0800

    added 15710

---
+++ www.spoj.com/15710:Iterated_sums/15710.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	if a > b {
+		a, b = b, a
+	}
+	sum := 0
+	for i := a; i <= b; i++ {
+		sum += i*i
+	}
+	fmt.Println(sum)
+}
