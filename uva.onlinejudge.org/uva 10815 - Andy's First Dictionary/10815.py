dc7f2b317fb265dfcae3e363f2fce0b7feb1e92c
frank <could.net@gmail.com>
2017-03-29 20:54:49 +0800

    added 10185

---
+++ uva.onlinejudge.org/uva 10815 - Andy's First Dictionary/10815.py
@@ -0,0 +1,46 @@
+
+def find_first_of_char(index, line):
+    i = index
+    while i < len(line):
+        if line[i].isalpha():
+            return i
+        else:
+            i += 1
+    return len(line)
+
+def find_first_notof_char(index, line):
+    i = index
+    while i < len(line):
+        if not line[i].isalpha():
+            return i
+        else:
+            i += 1
+    return len(line)
+
+def get_words(line, words):
+    start = 0
+    while start < len(line):
+        start = find_first_of_char(start, line)
+        end = find_first_notof_char(start, line)
+        if start < len(line):
+            word = line[start:end]
+            words.add(word.lower())
+            start = end
+
+
+if __name__ == '__main__':
+    words = set()
+    try:
+        while True:
+            line = input().strip()
+            if not line:
+                continue
+            get_words(line, words)
+    except(EOFError):
+        pass
+    
+    l = list(words)
+    l.sort()
+    for w in l:
+        print(w)
+
