2b94503aa20c679278a3370086bd9961d91fa7cd
frank <could.net@gmail.com>
2017-03-31 11:17:18 +0800

    added 488

---
+++ uva.onlinejudge.org/uva 488 - Triangle Wave/488.py
@@ -0,0 +1,42 @@
+from _curses import pair_content
+
+def get_pair():
+    pair = []
+    while True:
+        try:
+            line = input()
+            if not line:
+                continue
+            
+            pair.append(int(line))
+            if len(pair) == 2:
+                a, b = pair
+                pair = []
+                yield a, b
+        except(EOFError):
+            break
+        
+
+def solve(amplitude, frequency):
+    l = [i for i in range(1, amplitude+1)]
+    l2 = l[-2::-1]
+    l.extend(l2)
+    for i in range(frequency):
+        if i != 0:
+            print()
+            print()
+        for index, x in enumerate(l):
+            if index != 0:
+                print()
+            print(str(x)*x, end="")
+
+cases = int(input())
+index = 0
+for a, b in get_pair():
+    if index > 0:
+        print()
+        print()
+    index += 1
+    solve(a, b)
+
+print()
