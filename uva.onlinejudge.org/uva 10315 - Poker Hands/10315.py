6c703e4167bedc002538ec3fdddc75b13065c986
frank <could.net@gmail.com>
2017-04-11 19:13:11 +0800

    source shorted

--- uva.onlinejudge.org/uva 10315 - Poker Hands/10315.py
+++ uva.onlinejudge.org/uva 10315 - Poker Hands/10315.py
@@ -2,10 +2,7 @@ from collections import defaultdict
 
 def GetValue(v):
     d = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-    if v in d:
-        return d[v]
-    
-    return int(v)
+    return int(v) if v.isdigit() else d[v]
 
 class TexasFactory:
     def __init__(self, five_list):
@@ -19,7 +16,7 @@ class TexasFactory:
             self.values[num] += 1
             self.suits[suit] += 1
             
-    def CheckStraight(self):
+    def _CheckStraight(self):
         vl = self.nums
         vl.sort(reverse=True)
         for i in range(1, len(vl)):
@@ -30,51 +27,48 @@ class TexasFactory:
         
     def CreateTexas(self):
         if len(self.suits) == 1: #only one suit
-            if self.CheckStraight():
-                return StraightFlush(self.nums)
+            if self._CheckStraight():
+                return StraightFlush(self.nums, 9)
             else:
-                return Flush(self.nums)
+                return Flush(self.nums, 6)
 
         if 4 in self.values.values():
-            return FourOfAKind(self.nums)
+            return FourOfAKind(self.nums, 8)
         
         if 3 in self.values.values() and 2 in self.values.values():
-            return FullHouse(self.nums)
+            return FullHouse(self.nums, 7)
         
-        if self.CheckStraight():
-            return Straight(self.nums)
+        if self._CheckStraight():
+            return Straight(self.nums, 5)
         
         if 3 in self.values.values():
-            return ThreeOfAKind(self.nums)
+            return ThreeOfAKind(self.nums, 4)
         
         pair_count = [i for i in self.values.values()].count(2)
         if pair_count == 2:
-            return TwoPairs(self.nums)
+            return TwoPairs(self.nums, 3)
         elif pair_count == 1:
-            return Pair(self.nums)
+            return Pair(self.nums, 2)
         
-        return HighCard(self.nums)
+        return HighCard(self.nums, 1)
         
 
 def ToString(texas):
     return '{}, cards:{}'.format(texas.__class__.__name__, texas.values)
         
-class StraightFlush:
-    def __init__(self, vl):
-        self.priority = 9
+        
+class Texas:
+    def __init__(self, vl, pri):
+        self.priority = pri
         self.values = vl
-        self.Sort()
-    
-    def Sort(self):
+        self._Sort()
+        
+class StraightFlush(Texas):    
+    def _Sort(self):
         self.values.sort(reverse=True)
         
-class FourOfAKind:
-    def __init__(self, vl):
-        self.priority = 8
-        self.values = vl
-        self.Sort()
-    
-    def Sort(self):
+class FourOfAKind(Texas):
+    def _Sort(self):
         d = defaultdict(int)
         for v in self.values:
             d[v] += 1
@@ -86,13 +80,8 @@ class FourOfAKind:
                 self.values[:4] = [k, k, k, k]
                 
 
-class FullHouse:
-    def __init__(self, vl):
-        self.priority = 7
-        self.values = vl
-        self.Sort()
-        
-    def Sort(self):
+class FullHouse(Texas):        
+    def _Sort(self):
         d = defaultdict(int)
         for v in self.values:
             d[v] += 1
@@ -103,25 +92,16 @@ class FullHouse:
             else:
                 self.values[-2:] = [k, k]
                 
-class Flush:
-    def __init__(self, vl):
-        self.priority = 6
-        self.values = vl
+class Flush(Texas):
+    def _Sort(self):
         self.values.sort(reverse=True)
         
-class Straight:
-    def __init__(self, vl):
-        self.priority = 5
-        self.values = vl
+class Straight(Texas):
+    def _Sort(self):
         self.values.sort(reverse=True)
         
-class ThreeOfAKind:
-    def __init__(self, vl):
-        self.priority = 4
-        self.values = vl
-        self.Sort()
-        
-    def Sort(self):
+class ThreeOfAKind(Texas):   
+    def _Sort(self):
         d = defaultdict(int)
         for v in self.values:
             d[v] += 1
@@ -135,31 +115,21 @@ class ThreeOfAKind:
         singles.sort(reverse=True)
         self.values[-2:] = singles        
 
-class TwoPairs:
-    def __init__(self, vl):
-        self.priority = 3
-        self.values = vl
-        self.Sort()
-        
-    def Sort(self):
+class TwoPairs(Texas):
+    def _Sort(self):
         d = defaultdict(int)
         for v in self.values:
             d[v] += 1
         
-        ps = [k for k, v in d.items() if v == 2]
+        ps = [k for k, v in d.items() if v == 2] # pairs value
         single = [k for k, v in d.items() if v == 1][0]
         ps.sort(reverse=True)
         self.values[0:2] = ps[0], ps[0]
         self.values[2:2] = ps[1], ps[1]
         self.values[-1] = single
         
-class Pair:
-    def __init__(self, vl):
-        self.priority = 2
-        self.values = vl
-        self.Sort()
-        
-    def Sort(self):
+class Pair(Texas):
+    def _Sort(self):
         d = defaultdict(int)
         for v in self.values:
             d[v] += 1
@@ -173,10 +143,8 @@ class Pair:
         singles.sort(reverse=True)
         self.values[-3:] = singles
 
-class HighCard:
-    def __init__(self, vl):
-        self.priority = 1
-        self.values = vl
+class HighCard(Texas):
+    def _Sort(self):
         self.values.sort(reverse=True)
 
 
