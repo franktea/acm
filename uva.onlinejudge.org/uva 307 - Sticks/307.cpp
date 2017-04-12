b662592f3b4f9e978b07351654345c4f0e219e04
frank <could.net@gmail.com>
2017-04-12 17:21:18 +0800

    tl2

--- uva.onlinejudge.org/uva 307 - Sticks/307.cpp
+++ uva.onlinejudge.org/uva 307 - Sticks/307.cpp
@@ -74,8 +74,20 @@ bool Stick::Recursive(const int32_t index, const int32_t sum, const int32_t leng
         {
             index_used[i] = 0;
             last_slice_length = slices[i];
-            if(i == slices.size()-1)
-                return false;
+            if(sum == 0)
+            {
+                bool first_longest = true;
+                for(size_t used_index = 1; used_index < i; ++used_index)
+                {
+                    if(! index_used[used_index])
+                    {
+                        first_longest = false;
+                        break;
+                    }
+                }
+                if(first_longest) return false;
+            }
+            if(slices[i] == slices[slices.size()-1]) return false;
         }
         else
             return true;
