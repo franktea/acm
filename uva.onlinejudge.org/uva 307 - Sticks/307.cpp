74e72b8796335b1dd4b787a34bcf207d1b739f69
frank <could.net@gmail.com>
2017-04-17 15:19:28 +0800

    added comments

--- uva.onlinejudge.org/uva 307 - Sticks/307.cpp
+++ uva.onlinejudge.org/uva 307 - Sticks/307.cpp
@@ -19,9 +19,9 @@ class Stick
 public:
     Stick(int32_t slice_count)
     {
-        slices.reserve(slice_count+1);
-        indexes.resize(slice_count+1, 0);
-        index_used.resize(slice_count+1, 0);
+        slices.reserve(slice_count+1); // 原始数据，每一段的长度
+        indexes.resize(slice_count+1, 0); // 递归的时候使用，每一段被选中的在slices数组中的下标，也是最终排列的结果，只是本题不要求输出此结果
+        index_used.resize(slice_count+1, 0); // 递归的时候使用，记录每一段是否使用过
         slices.push_back(0);
     }
 
@@ -61,7 +61,7 @@ bool Stick::Recursive(const int32_t index, const int32_t sum, const int32_t leng
         return true;
     }
 
-    size_t from_index = sum == 0 ? 1 : indexes[index-1] + 1;
+    size_t from_index = sum == 0 ? 1 : indexes[index-1] + 1; //如果不是小棍的第一截，则后面每一段的下标一定比前面的大，加上这一句以后优化了非常多
     for(size_t i = from_index; i < slices.size();++i)
     {
         if(0 != index_used[i] ||
@@ -71,8 +71,9 @@ bool Stick::Recursive(const int32_t index, const int32_t sum, const int32_t leng
         if(! Recursive(index+1, sum + slices[i] == length ? 0 : sum + slices[i], length))
         {
             index_used[i] = 0;
-            if(sum == 0) return false;
-            while(i+1 < slices.size() && slices[i+1] == slices[i]) ++i;
+            if(sum == 0) return false; // 如果第一段都失败了，则整根肯定都失败了
+            //if(slices[i] == slices[slices.size()-1]) return false; // 如果最短的都用掉了，则不用再试了
+            while(i+1 < slices.size() && slices[i+1] == slices[i]) ++i; // 如果当前长度失败了，则后面相同的长度全部跳过
         }
         else
             return true;
