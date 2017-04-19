094bfe4662758095cb308b6f719889293c722838
frank <could.net@gmail.com>
2017-04-19 20:12:59 +0800

    10071

---
+++ uva.onlinejudge.org/uva 10071 - Back to High School Physics/10071.cpp
@@ -0,0 +1,20 @@
+/*
+ * 10071.cpp
+ *
+ *  Created on: Apr 19, 2017
+ *      Author: frank
+ */
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int main()
+{
+	int32_t a, b;
+	while(cin>>a>>b)
+		cout<<2*a*b<<"\n";
+	return 0;
+}
+
+
+
