177a4ea38bc6f016748368f1174318e97a326414
frank <could.net@gmail.com>
2017-04-19 20:39:23 +0800

    10469

---
+++ uva.onlinejudge.org/uva 10469 - To Carry or not to Carry/10469.cpp
@@ -0,0 +1,20 @@
+/*
+ * 10469.cpp
+ *  uva 10469 - To Carry or not to Carry
+ *  Created on: Apr 19, 2017
+ *      Author: frank
+ */
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int main()
+{
+	int32_t a, b;
+	while(cin>>a>>b)
+		cout<<(a^b)<<"\n";
+	return 0;
+}
+
+
+
