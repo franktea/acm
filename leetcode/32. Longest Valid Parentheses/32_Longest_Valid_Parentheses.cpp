8903a33f2cd9004ab91639e409f18a3f687789ec
frank <could.net@gmail.com>
2018-08-24 21:38:00 +0800

    pass but slow

--- leetcode/32. Longest Valid Parentheses/32_Longest_Valid_Parentheses.cpp
+++ leetcode/32. Longest Valid Parentheses/32_Longest_Valid_Parentheses.cpp
@@ -9,47 +9,105 @@
 #include <string>
 #include <list>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 class Solution {
+	struct Range
+	{
+		int start;
+		int ending;
+		bool operator<(const Range& another) const
+		{
+			return start < another.start;
+		}
+
+		int Length()
+		{
+			return ending - start + 1;
+		}
+
+		// make sure *this < another
+		bool OverLap(const Range& another) const
+		{
+			return (this->ending + 1 == another.start) ||
+					(this->ending > another.ending);
+		}
+
+		void Merge(const Range& another)
+		{
+			this->ending = std::max(this->ending, another.ending);
+		}
+	};
+
+	struct Pos
+	{
+		int index;
+		char c;
+	};
 public:
     int longestValidParentheses(string s)
     {
-    	int max_len = 0;
-    	int current_len = 0;
-    	list<char> q; // work as a queue
-    	for(char c: s)
+    	list<Pos> q; // work as a queue
+    	vector<Range> ranges;
+    	for(int i = 0; i < s.length(); ++i)
     	{
-    		if(c == '(')
+    		if(s[i] == ')')
     		{
-    			q.push_back(c);
-    		}
-    		else // c == ')'
-    		{
-    			if(q.back() == '(')
-    			{
-    				q.pop_back();
-    				current_len += 2;
-    				max_len = std::max(current_len, max_len);
-    			}
-    			else // invalid
+    			if(q.back().c != '(')
     			{
     				q.clear();
-    				current_len = 0;
     			}
+    			else
+    			{
+    				ranges.push_back({q.back().index, i});
+    				q.pop_back();
+    			}
+    		}
+    		else // s[i] == '('
+    		{
+    			q.push_back({i, s[i]});
     		}
     	}
 
-    	return max_len;
+    	if(ranges.empty())
+    		return 0;
+
+    	sort(ranges.begin(), ranges.end());
+
+    	size_t current_index = 0;
+    	int max_length = ranges[0].Length();
+    	for(size_t i = 1; i < ranges.size(); ++i)
+    	{
+    		if(current_index == i)
+    			continue;
+
+    		if(ranges[current_index].OverLap(ranges[i]))
+    		{
+    			ranges[current_index].Merge(ranges[i]);
+    		}
+    		else
+    		{
+    			max_length = std::max(max_length, ranges[current_index].Length());
+    			current_index = i;
+    		}
+    	}
+    	max_length = std::max(max_length, ranges[current_index].Length());
+
+//    	for(auto r: ranges)
+//    	{
+//    		cout<<r.start<<", "<<r.ending<<"\n";
+//    	}
+
+    	return max_length;
     }
 };
 
 int main()
 {
 	Solution s;
-	assert(s.longestValidParentheses("(()") == 2);
-	assert(s.longestValidParentheses(")()())") == 4);
-	cout<<s.longestValidParentheses("(()()(()()()")<<"\n";
+	cout<<s.longestValidParentheses("((())()")<<"\n";
+	cout<<s.longestValidParentheses("(()()((()()())")<<"\n";
 	return 0;
 }
