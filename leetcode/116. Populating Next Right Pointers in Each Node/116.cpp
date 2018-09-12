cd39184d7eee1afcd0c8a05d7933d9b954254bea
frank <could.net@gmail.com>
2018-09-12 16:18:08 +0800

    116 wrong

---
+++ leetcode/116. Populating Next Right Pointers in Each Node/116.cpp
@@ -0,0 +1,46 @@
+/*
+ * 116.cpp
+ *
+ *  Created on: Sep 12, 2018
+ *      Author: frank
+ */
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+//Definition for binary tree with next pointer.
+struct TreeLinkNode {
+	int val;
+ 	TreeLinkNode *left, *right, *next;
+ 	TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ };
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+    	if(!root) return;
+
+    	queue<std::pair<int, TreeLinkNode*>> bfs;
+    	bfs.push({0, root});
+    	while(bfs.empty())
+    	{
+    		auto p = bfs.front();
+    		bfs.pop();
+    		if(bfs.empty() || bfs.front().first != p.first)
+    			p.second->next = nullptr;
+    		else
+    			p.second->next = bfs.front().second;
+
+    		if(p.second->left)
+    			bfs.push({p.first+1, p.second->left});
+    		if(p.second->right)
+    			bfs.push({p.first+1, p.second->right});
+    	}
+    }
+};
+
+
+
+
+
