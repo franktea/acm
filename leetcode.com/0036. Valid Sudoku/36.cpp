0a69ff53951240d4ac8fdd29d1f005dfb2f283a1
frank <could.net@gmail.com>
2018-09-16 22:23:13 +0800

    36

---
+++ leetcode.com/0036. Valid Sudoku/36.cpp
@@ -0,0 +1,23 @@
+/*
+ * 36.cpp
+ *
+ *  Created on: Sep 16, 2018
+ *      Author: frank
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+    }
+private:
+    bool DFS()
+    {
+
+    }
+};
