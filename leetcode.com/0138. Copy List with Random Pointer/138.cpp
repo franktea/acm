5438937f4925fe3adda39d4179488a10c6a0ab94
frank <could.net@gmail.com>
2020-06-04 21:06:41 +0800

    一点问题

--- leetcode.com/0138. Copy List with Random Pointer/138.cpp
+++ leetcode.com/0138. Copy List with Random Pointer/138.cpp
@@ -12,35 +12,44 @@ using namespace std;
 // 用map保存random指针的映射，提交不通过。这题很无聊，暂时不想找原因。
 
 // Definition for singly-linked list with a random pointer.
-struct RandomListNode {
-	int label;
-	RandomListNode *next, *random;
-	RandomListNode(int x, RandomListNode* n = nullptr) : label(x), next(n), random(NULL) {}
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
 };
 
 class Solution {
 public:
-    RandomListNode *copyRandomList(RandomListNode *head) {
+    Node *copyRandomList(Node *head) {
     	if(!head) return nullptr;
-    	RandomListNode* h = head;
-    	map<RandomListNode*, RandomListNode*> pointer_map;
-    	RandomListNode* ret = new RandomListNode(head->label);
+    	Node* h = head;
+    	map<Node*, Node*> pointer_map; // 用map没有任何问题，只是之前的对应关系没搞对
+    	Node* ret = new Node(head->val);
     	pointer_map.insert({head, ret});
-    	RandomListNode* head2 = ret;
+    	Node* head2 = ret;
     	head = head->next;
     	while(head)
     	{
-    		RandomListNode* p = new RandomListNode(head->label);
+    		Node* p = new Node(head->val);
     		pointer_map.insert({head, p});
     		head2->next = p;
     		head2 = head2->next;
     		head = head->next;
     	}
 
-    	RandomListNode* h2 = ret;
+    	Node* h2 = ret;
     	while(h)
-    	{
-    		h2->random = pointer_map[h];
+    	{	
+			if(h->random)
+    			h2->random = pointer_map[h->random];
+
     		h2 = h2->next;
     		h = h->next;
     	}
@@ -51,19 +60,6 @@ public:
 
 int main()
 {
-	using ln = RandomListNode;
-	ln* head = new ln(1, new ln(2));
-	head->random = head;
-	head->next->random = head;
-	Solution* ps = new Solution;
-	ln* h = ps->copyRandomList(head);
-	cout<<":::::"<<h->random->label<<"\n";
-	while(h)
-	{
-		cout<<h->label<<", ";
-		h = h->next;
-	}
-	cout<<"\n";
 	return 0;
 }
 
